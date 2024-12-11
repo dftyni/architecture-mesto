15721fc6f42f0e6fb8f126d871583ecad9187122
Semenov <assemenov@cloud.ru>
2024-12-11 20:49:43 +0300

    Task 1 init

---
+++ mongo-sharding-repl/scripts/4_fill_it_with_test_data.sh
@@ -0,0 +1,10 @@
+###
+# Наполните его тестовыми данными
+###
+
+docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+db.helloDoc.countDocuments()
+exit();
+EOF
