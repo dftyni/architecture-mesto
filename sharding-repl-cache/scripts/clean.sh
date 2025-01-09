38476e60c18692a83b7b1b0679fa6f599ac05e17
Vyacheslav Baglay <terra-n3@yandex.ru>
2025-01-09 23:04:44 +0300

    working cache

---
+++ sharding-repl-cache/scripts/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo docker stop configSrv
+sudo docker stop shard1
+sudo docker stop shard2
+sudo docker stop mongos_router
+sudo docker stop pymongo_api
+sudo docker stop shard1a
+sudo docker-compose down --volumes
+sudo docker volume rm architecture-sprint-2_mongodb1_data_container
+sudo docker volume rm mongo-sharding_config-data
+sudo docker volume rm mongo-sharding_shard1-data
+sudo docker volume rm mongo-sharding_shard2-data
+sudo docker volume rm mongo-sharding_shard1a-data
+sudo docker volume rm mongo-sharding_shard1b-data
+sudo docker volume rm mongo-sharding_shard2a-data
+sudo docker volume rm mongo-sharding_shard2b-data
+sudo docker volume rm portainer_data
+sudo docker volume rm redis_1_data
+sudo docker system prune --volumes
+sudo docker volume ls
