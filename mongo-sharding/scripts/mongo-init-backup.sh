de103b767e27a7a37ab33a015eacf405dc8d8c65
PNdvg <pavel.nedviga@gmail.com>
2025-02-23 22:31:14 +0300

    Задание 2. Шардирование + схема

---
+++ mongo-sharding/scripts/mongo-init-backup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+set -ex  # -e: остановка при ошибке, -x: вывод всех команд
+
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+quit();
+EOF
+
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
+
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+);
+quit();
+EOF
+
+docker compose exec -T shard2 mongosh --port 27019 <<EOF
+
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+quit(); 
+EOF
+
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+
+sh.enableSharding("somedb");
+db.helloDoc.drop();
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+use somedb;
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments();
+quit(); 
+EOF
