e92734de194a9818c1b115b85085c24571050375
Mikhail Ponkin <mponkin@gmail.com>
2025-02-24 19:44:01 +0300

    completed sharding with replication

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -14,21 +14,31 @@ EOF
 echo "---------------------"
 echo "Initializing shard1"
 echo "---------------------"
-docker compose exec -T shard1 mongosh --port 27018 <<EOF
+docker compose exec -T shard1-1 mongosh --port 27018 <<EOF
 rs.initiate({
-    _id: "shard1",
-    members: [{_id: 0, host: "shard1:27018"}]
-})
+    _id: "rs1", 
+    members: 
+    [
+        {_id: 0, host: "shard1-1:27018"},
+        {_id: 1, host: "shard1-2:27021"},
+        {_id: 2, host: "shard1-3:27022"}
+    ]
+}) 
 EOF
 
 echo "---------------------"
 echo "Initializing shard2"
 echo "---------------------"
-docker compose exec -T shard2 mongosh --port 27019 <<EOF
+docker compose exec -T shard2-1 mongosh --port 27019 <<EOF
 rs.initiate({
-    _id : "shard2",
-    members: [{_id: 1, host: "shard2:27019"}]
-})
+    _id: "rs2", 
+    members: 
+    [
+        {_id: 0, host: "shard2-1:27019"},
+        {_id: 1, host: "shard2-2:27023"},
+        {_id: 2, host: "shard2-3:27024"}
+    ]
+}) 
 EOF
 
 echo "Wait 3 seconds..."
@@ -38,8 +48,8 @@ echo "---------------------"
 echo "Adding documents, testing overall count"
 echo "---------------------"
 docker compose exec -T mongos_router mongosh --port 27020 <<EOF
-sh.addShard("shard1/shard1:27018")
-sh.addShard("shard2/shard2:27019")
+sh.addShard("rs1/shard1-1:27018")
+sh.addShard("rs2/shard2-1:27019")
 sh.enableSharding("somedb")
 sh.shardCollection("somedb.helloDoc", {"name": "hashed"})
 use somedb
@@ -50,7 +60,7 @@ EOF
 echo "---------------------"
 echo "Testing shard1 documents count"
 echo "---------------------"
-docker compose exec -T shard1 mongosh --port 27018 <<EOF
+docker compose exec -T shard1-1 mongosh --port 27018 <<EOF
 use somedb
 db.helloDoc.countDocuments()
 EOF
@@ -58,7 +68,7 @@ EOF
 echo "---------------------"
 echo "Testing shard2 documents count"
 echo "---------------------"
-docker compose exec -T shard2 mongosh --port 27019 <<EOF
+docker compose exec -T shard2-1 mongosh --port 27019 <<EOF
 use somedb
 db.helloDoc.countDocuments()
 EOF
