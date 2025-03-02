e00156c361701b8d063cc4e2fc998793545f48c2
VitEr-dev <vit140775@gmail.com>
2025-03-02 04:14:43 +0300

    finishing task4

---
+++ sharding-repl-cache/scripts/mongo-init-mongos_router.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###
+# Инициализируем роутер
+###
+
+docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
+
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+exit();
+EOF 
