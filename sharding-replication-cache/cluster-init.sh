930b35f67d7bf490478a420161691d9912a9ed6c
Alexander Proshlyakov <tashimotor@gmail.com>
2025-03-22 09:50:58 +0300

    feat(caching): add caching with redis
    refs: https://practicum.yandex.ru/learn/software-architect/courses/dac4879e-9b22-46b2-8c44-175eaadb89e3/sprints/483107/topics/cb41c1a6-3c04-4a75-911d-82e250bb4956/lessons/0000549c-ae71-42fe-ba51-3b7c93e13ec9/

---
+++ sharding-replication-cache/cluster-init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+docker compose exec -T configSrv mongosh --port 27019 --quiet <<EOF
+rs.initiate(
+   {
+     _id : "config_server",
+     configsvr: true,
+     members: [
+       { _id : 0, host : "configSrv:27019" }
+     ]
+   }
+ );
+EOF
+
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+        { _id : 1, host : "shard1-secondary1:27018" },
+        { _id : 2, host : "shard1-secondary2:27018" }
+      ]
+    }
+);
+EOF
+
+docker compose exec -T shard2 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard2:27018" },
+        { _id : 1, host : "shard2-secondary1:27018" },
+        { _id : 2, host : "shard2-secondary2:27018" }
+      ]
+    }
+);
+EOF
+
